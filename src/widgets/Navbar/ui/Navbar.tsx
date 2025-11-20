3c2242ea23803f24997bb8bdb332b5e18e14fdc5
Dima Rui <dmytroruidev@gmail.com>
2025-11-20 18:46:27 +0200

    add navbar ui, logo

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,7 +1,20 @@
 import s from './Navbar.module.scss';
 
 import { classNames } from '@/shared/helpers/classNames/classNames';
+import { Icon } from '@/shared/ui/Icon';
+import VibenetLogo from '@/shared/assets/icons/vibenet-logo.svg';
 
 export function Navbar() {
-	return <div className={classNames(s.Navbar, {}, [])}>Navbar component</div>;
+	return (
+		<div className={classNames(s.Navbar, {}, [])}>
+			<div className={s.logo}>
+				<Icon
+					Svg={VibenetLogo}
+					width={60}
+					height={60}
+				/>
+				<p>Vibenet</p>
+			</div>
+		</div>
+	);
 }
