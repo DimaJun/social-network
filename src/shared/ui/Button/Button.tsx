5f4bd4ffe972cbc21a15649969a93fdba40d7649
Dmytro Rui <drankendmitrij@gmail.com>
2025-12-02 20:00:18 +0200

    updated: SidebarItem styles, reset.scss; added: Button, Input components

---
+++ src/shared/ui/Button/Button.tsx
@@ -0,0 +1,41 @@
+import { ButtonHTMLAttributes } from 'react';
+
+import s from './Button.module.scss';
+
+import { classNames } from '@/shared/helpers/classNames/classNames';
+
+type ButtonVariant = 'clear' | 'outline';
+
+const mapVariantToClass: Record<ButtonVariant, string> = {
+	clear: s.clear,
+	outline: s.outline,
+};
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+	className?: string;
+	variant?: ButtonVariant;
+}
+
+export function Button(props: ButtonProps) {
+	const {
+		className,
+		variant = 'clear',
+		type = 'button',
+		children,
+		disabled,
+		...otherProps
+	} = props;
+
+	const variantClass = mapVariantToClass[variant];
+
+	return (
+		<button
+			className={classNames(s.button, {}, [className, variantClass])}
+			type={type}
+			disabled={disabled}
+			{...otherProps}
+		>
+			{children}
+		</button>
+	);
+}
---
+++ src/shared/ui/Input/Input.tsx
@@ -0,0 +1,56 @@
+import { InputHTMLAttributes } from 'react';
+
+import s from './Input.module.scss';
+
+import { classNames } from '@/shared/helpers/classNames/classNames';
+
+type TypedInputProps = Omit<InputHTMLAttributes<HTMLInputElement>, 'onChange' | 'value'>;
+
+interface InputProps extends TypedInputProps {
+	className?: string;
+	value?: string;
+	onChange?: (value: string) => void;
+	label?: string;
+}
+
+export function Input(props: InputProps) {
+	const {
+		className,
+		value,
+		onChange,
+		label,
+		type = 'text',
+		disabled = false,
+		required = false,
+		...otherProps
+	} = props;
+
+	if (!label) {
+		return (
+			<input
+				className={classNames(s.input, {}, [className])}
+				type={type}
+				value={value}
+				onChange={(e) => onChange(e?.target?.value)}
+				disabled={disabled}
+				required={required}
+				{...otherProps}
+			/>
+		);
+	}
+
+	return (
+		<label className={s.label}>
+			<span>{label}</span>
+			<input
+				className={classNames(s.input, {}, [className])}
+				type={type}
+				value={value}
+				onChange={(e) => onChange(e?.target?.value)}
+				disabled={disabled}
+				required={required}
+				{...otherProps}
+			/>
+		</label>
+	);
+}
