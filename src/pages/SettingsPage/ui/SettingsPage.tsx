5e8d6e33344aadc868e92ae2bd830d8bb3f49baf
Dmytro Rui <drankendmitrij@gmail.com>
2025-12-07 21:00:44 +0200

    added: Switch theme on SettingsPage; changed: Switch styles

--- src/pages/SettingsPage/ui/SettingsPage.tsx
+++ src/pages/SettingsPage/ui/SettingsPage.tsx
@@ -3,15 +3,19 @@ import { Moon, Sun } from 'lucide-react';
 import s from './SettingsPage.module.scss';
 
 import { classNames } from '@/shared/helpers/classNames/classNames';
-import { Button } from '@/shared/ui/Button';
 import { useTheme } from '@/shared/hooks/useTheme/useTheme';
+import { Switch } from '@/shared/ui/Switch';
 
 export function SettingsPage() {
 	const { theme, toggleTheme } = useTheme();
 
 	return (
 		<div className={classNames(s.SettingsPage, {}, [])}>
-			<Button onClick={toggleTheme}>{theme === 'dark' ? <Moon /> : <Sun />}</Button>
+			<Switch
+				checked={theme === 'dark'}
+				onCheck={toggleTheme}
+				Icon={theme === 'dark' ? Moon : Sun}
+			/>
 		</div>
 	);
 }
